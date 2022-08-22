bd17b80df02cbdf48c4ccbf5e6bc7843783192a8
Mahfuzul Haque <68540132+MahfuzulHaque-22@users.noreply.github.com>
2022-08-22 23:33:19 +0600

    Add files via upload

---
+++ src/txt_filename.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MAHFUZ TASNIM
+ */
+class txt_filename {
+
+    static void setText(String filename) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
